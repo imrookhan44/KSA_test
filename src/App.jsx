e921ce8495dd4745adb1503d89fff07410c13733
shanda-studio <khannihar921@gmail.com>
2023-05-18 10:09:44 +0500

    fix  issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
 import './App.css'
 import Header from './components/header/Header'
-import Login from './components/login/Login'
-import Profile from './components/profile/Profile'
-import ResetPassword from './components/resepassword/ResetPassword'
-import Reset from './components/resetpassword/Reset'
-import SignUp from './components/signup/SIgnup'
-
 function App() {
-
   return (
     <>
       <Header />
-      {/* <Reset /> */}
-      {/* <Login /> */}
-      {/* <ResetPassword /> */}
-      {/* <SignUp /> */}
-      {/* <Profile /> */}
+
     </>
   )
 }
